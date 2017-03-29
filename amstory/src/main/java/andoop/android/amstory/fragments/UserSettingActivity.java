0c5e8a8169041ea891cdbfeecc5b542fdd943d59
wangfeilong <418593670@qq.com>
2017-03-29 19:02:14 +0800

    修改个人页面

--- amstory/src/main/java/andoop/android/amstory/fragments/UserSettingActivity.java
+++ amstory/src/main/java/andoop/android/amstory/fragments/UserSettingActivity.java
@@ -11,5 +11,6 @@ public class UserSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_setting);
+        
     }
 }
