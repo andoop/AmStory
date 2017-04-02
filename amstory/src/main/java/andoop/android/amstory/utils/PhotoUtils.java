d57a2896cf6c0b12f9f1681fc1e30f329a5c1c7c
wangfeilong <418593670@qq.com>
2017-04-02 23:19:48 +0800

    添加拍照和从相册选取功能

---
+++ amstory/src/main/java/andoop/android/amstory/utils/PhotoUtils.java
@@ -0,0 +1,8 @@
+package andoop.android.amstory.utils;
+
+/**
+ * Created by Administrator on 2017/4/2.
+ */
+
+public class PhotoUtils {
+}
---
+++ amstory/src/main/java/andoop/android/amstory/utils/UILImageLoader.java
@@ -0,0 +1,7 @@
+package andoop.android.amstory.utils;
+
+/**
+ * Created by Administrator on 2017/3/29.
+ */
+public class UILImageLoader {
+}
